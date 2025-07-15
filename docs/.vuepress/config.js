c5a2f5f460090c6b4e36544dad9e4766853c5229
Johannes Skov Frandsen <jsf@greenoak.dk>
2025-07-15 09:30:49 +0000

    Fixed dependency issue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,12 @@
-module.exports = {
-    lang: 'en-US',
-    title: 'Karla',
-    description: 'Php ImageMagick wrapper with method chaining',
-    base: "/karla/",
-}
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  bundler: viteBundler(),
+  theme: defaultTheme(),
+  lang: 'en-US',
+  title: 'Karla',
+  description: 'Php ImageMagick wrapper with method chaining',
+  base: "/karla/",
+})
